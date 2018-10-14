827ac1a1c48d52ec6340f811ca1279b24533a953
Weihua4455 <39421762+Weihua4455@users.noreply.github.com>
2018-10-14 17:00:31 -0400

    first two charts

--- src/01-map.js
+++ src/01-map.js
@@ -14,3 +14,68 @@ let svg = d3
   .attr('width', width + margin.left + margin.right)
   .append('g')
   .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+let projection = d3.geoMercator()
+let graticule = d3.geoGraticule()
+
+let path = d3.geoPath().projection(projection)
+
+var colorScale = d3.scaleSequential(d3.interpolateViridis).clamp(true)
+
+Promise.all([
+  d3.json(require('./data/world.topojson')),
+  d3.csv(require('./data/world-cities.csv'))
+])
+  .then(ready)
+  .catch(err => console.log('Failed on', err))
+
+function ready([json, cities]) {
+  // console.log(json.objects)
+  // console.log(cities)
+
+  let countries = topojson.feature(json, json.objects.countries)
+  // console.log(countries)
+
+  // let allPop = cities.map(d => +d.population)
+  colorScale.domain([0, 1000000])
+  // colorScale.domain(d3.extent(allPop))
+  svg
+    .selectAll('.country')
+    .data(countries.features)
+    .enter()
+    .append('path')
+    .attr('class', 'country')
+    .attr('d', path)
+    .attr('stroke', 'black')
+    .attr('fill', 'black')
+
+  svg
+    .append('path')
+    .datum(graticule())
+    .attr('d', path)
+    .attr('stroke', 'gray')
+    .attr('fill', 'none')
+    .lower()
+
+  svg
+    .append('rect')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('fill', 'black')
+    .lower()
+
+  svg
+    .selectAll('.cities')
+    .data(cities)
+    .enter()
+    .append('circle')
+    .attr('class', 'cities')
+    .attr('r', 1)
+    .attr('transform', d => {
+      // console.log(d)
+      var coords = [d.lng, d.lat]
+      // console.log(coords)
+      return `translate(${projection(coords)})`
+    })
+    .attr('fill', d => colorScale(d.population))
+}
