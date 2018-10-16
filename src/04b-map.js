68381ec3dcba891aeeb8340c654115e4232bd852
Weihua4455 <39421762+Weihua4455@users.noreply.github.com>
2018-10-16 01:50:27 -0400

    10-16-18

--- src/04b-map.js
+++ src/04b-map.js
@@ -14,3 +14,54 @@ let svg = d3
   .attr('width', width + margin.left + margin.right)
   .append('g')
   .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+let projection = d3.geoAlbersUsa()
+
+let path = d3.geoPath().projection(projection)
+
+let opacityScale = d3.scaleLinear().range([0.025, 1])
+
+d3.json(require('./data/counties_with_election_data.topojson'))
+  .then(ready)
+  .catch(err => console.log('Failed on', err))
+
+function ready(json) {
+  // console.log(json)
+
+  let counties = topojson.feature(json, json.objects.us_counties)
+
+  let clintonVoter = counties.features.map(d => +d.properties.clinton)
+  let trumpVoter = counties.features.map(d => +d.properties.trump)
+  let voter = (d3.sum(clintonVoter) + d3.sum(trumpVoter)) / 500
+
+  opacityScale.domain([0, voter])
+
+  // let pop = counties.features.map(
+  //   d => +d.properties.clinton + +d.properties.trump
+  // )
+
+  // opacityScale.domain(d3.extent(pop))
+
+  svg
+    .selectAll('.country')
+    .data(counties.features)
+    .enter()
+    .append('path')
+    .attr('class', 'country')
+    .attr('d', path)
+    .attr('stroke', 'white')
+    .attr('fill', d => {
+      if (d.properties.clinton > d.properties.trump) {
+        return 'blue'
+      } else {
+        return 'red'
+      }
+    })
+    .style('opacity', d => {
+      if (d.properties.clinton > d.properties.trump) {
+        return opacityScale(d.properties.clinton)
+      } else {
+        return opacityScale(d.properties.trump)
+      }
+    })
+}
